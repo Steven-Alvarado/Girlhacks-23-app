dcd4695d032c9e1f41312ffa28605742cccef390
Steven-Alvarado <115261449+Steven-Alvarado@users.noreply.github.com>
2023-09-23 23:38:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 
 st.title("Space Satellite Coordinator")
 
-st.image("images/Weather Satellite Images_ If the Earth Took a Selfie.jpeg", width=200)
+st.image("images/Weather Satellite Images_ If the Earth Took a Selfie.jpeg", width=300)
 
 # Function to get satellite info based on location
 def get_satellite_info(location):
